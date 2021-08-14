c994fa00e96a10840202dd32ea5456ab2e2581a0
na7esan <na7esan@gmail.com>
2021-08-14 18:41:32 +0900

    ArmorFactory作成

---
+++ database/factories/ArmorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Armor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArmorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Armor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}

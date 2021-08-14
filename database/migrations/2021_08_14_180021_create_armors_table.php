0868aaa61d26a1104091ac8e6a16fd44ee842c88
na7esan <na7esan@gmail.com>
2021-08-14 18:04:01 +0900

    armorsマイグレにnameカラムを追加

--- database/migrations/2021_08_14_180021_create_armors_table.php
+++ database/migrations/2021_08_14_180021_create_armors_table.php
@@ -15,6 +15,7 @@ class CreateArmorsTable extends Migration
     {
         Schema::create('armors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }

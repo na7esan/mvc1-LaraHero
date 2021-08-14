604d7d903e2178b1ec632d6db6ee07c3fa4d69be
na7esan <na7esan@gmail.com>
2021-08-14 17:46:18 +0900

    武器あり勇者2のhero2Appearの処理を記述

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -15,7 +15,13 @@ class HeroController extends Controller
     {
         // 最初にルーティングファイルとコントローラファイルが繋がっているか確認する
         // この作業を行っておけば、ルーティングとコントローラが上手く繋がらないというエラーを最初に潰しておける
-        return 'hero2';
+        // return 'hero2';
 
+        // 繋がっていることが確認できたらアクションに実際の処理内容を記述していく
+        // 今回は武器であるviewファイルを呼び出す処理を記述する
+        // 拡張機能であるLaravel Create Viewを入れておくと便利
+        // https://marketplace.visualstudio.com/items?itemName=glitchbl.laravel-create-view
+
+        return view('hero2');
     }
 }

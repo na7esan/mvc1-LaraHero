5e7e5397509dd913a7df09da4ae078ada77bbfe1
na7esan <na7esan@gmail.com>
2021-08-15 00:44:41 +0900

    hero2のビューファイル作成

--- resources/views/hero2.blade.php
+++ resources/views/hero2.blade.php
@@ -1,1 +1,11 @@
-武器あり勇者2のビューファイル
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>武器あり勇者2のページ</title>
+    </head>
+    <body>
+        <p>これは勇者2の物語である</p>
+    <p>武器あり勇者2のビューファイル</p>
+</body>
+</html>

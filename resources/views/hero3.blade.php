c9003135aaaab2381e5b0cff124a7807484b3a7d
na7esan <na7esan@gmail.com>
2021-08-15 00:40:03 +0900

    hero3のビュー作成

--- resources/views/hero3.blade.php
+++ resources/views/hero3.blade.php
@@ -1,3 +1,11 @@
-@php
-    dd($armor);
-@endphp
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>武器防具あり勇者３のページ</title>
+</head>
+<body>
+    <p>これは勇者3の物語である</p>
+    <p>勇者3は{{ $armor->name }}を装備した</p>
+</body>
+</html>
